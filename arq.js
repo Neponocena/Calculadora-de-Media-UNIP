d5ad11d9611db11e6292b086c4161e346b553c1e
Neponocena <pedronepo2005@gmail.com>
2026-02-02 09:46:50 -0300

    refactor: organiza validações, e uso do let/const

--- arq.js
+++ arq.js
@@ -1,14 +1,39 @@
 function calcular()
     {
-        var np1 = Number(document.getElementById("np1").value)
-        var np2 = Number(document.getElementById("np2").value)
-        var pim = Number(document.getElementById("pim").value)
+       let np1 = document.getElementById("np1").value
+       let np2 = document.getElementById("np2").value
+       let pim = document.getElementById("pim").value
+       const res = document.getElementById("res")
+    
+    if (np1 === "" || np2 === "" || pim === "")
+        {
+            res.innerHTML = "Preencha todos os campos"
+            return
+        }
 
-        var media = (np1 * 0.4) + (np2 * 0.4) + (pim * 0.2);
+         np1 = Number(np1)
+         np2 = Number(np2)
+         pim = Number(pim)
 
-        var res = document.getElementById("res")
+        if(isNaN(np1) || isNaN(np2) || isNaN(pim))
+            {
+                res.innerHTML = "Preencha todas as notas corretamente"
+                return
+            }
 
-        res.innerHTML = `Sua média semestral é ${media} `
+        if
+        (
+            np1 < 0 || np1 > 10 ||
+            np2 < 0 || np2 > 10 ||
+            pim < 0 || pim > 10 
+        )
+        {
+          res.innerHTML = "As notas devem estar entre 0 e 10."
+          return
+        }
+
+        let media = (np1 * 0.4) + (np2 * 0.4) + (pim * 0.2);
+        
 
         if(media >= 7)
         {
